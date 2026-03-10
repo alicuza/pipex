f56d56fbab704390fd38dc27f2d058cd32f8a065
Stefan-Emanuel Ancuta <sancuta@c3r7p3.42vienna.com>
2026-03-10 12:33:34 +0100

    possible tokens for minishell

--- pipex.h
+++ pipex.h
@@ -6,34 +6,39 @@
 /*   By: sancuta <sancuta@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 07:40:36 by sancuta           #+#    #+#             */
-/*   Updated: 2026/03/09 12:26:07 by sancuta          ###   ########.fr       */
+/*   Updated: 2026/03/10 12:31:53 by sancuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-typedef enum e_type
+typedef enum e_token
 {
-	TYPE_NIL;
-	TYPE_CMD;
-	TYPE_ARG;
-}	t_type;
+	TOK_NIL,
+	TOK_AND,
+	TOK_OR,
+	TOK_PIPE,
+	TOK_WORD,
+	TOK_HERE_DOC,
+	TOK_RED_IN,
+	TOK_RED_OUT,
+}	t_token;
 
 typedef struct s_thing
 {
-	t_type	type;
-	char	*data;
-	size_t	parent_idx;
+	t_token	type;
+	size_t	data_idx;
+	size_t	data_cnt;
 	size_t	sibling_idx;
 	size_t	child_idx;
 }	t_thing;
 
 typedef struct s_env
 {
-	t_arena	perm;
-	t_arena	temp;
-	t_thing	*nodes;
+	t_arena	nodes;
+	t_arena	tmp;
 	size_t	pipe_head;
 }	t_env;
+
 #endif
