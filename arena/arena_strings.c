fb8d9b53d34db2ef6ca0e7e0635ffa0e2180e94d
Stefan-Emanuel Ancuta <sancuta@c1r4p1.42vienna.com>
2026-03-18 13:22:20 +0100

    fixing arenas slowly

--- arena/arena_strings.c
+++ arena/arena_strings.c
@@ -6,7 +6,7 @@
 /*   By: sancuta <sancuta@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/15 20:05:14 by sancuta           #+#    #+#             */
-/*   Updated: 2026/03/18 12:33:23 by sancuta          ###   ########.fr       */
+/*   Updated: 2026/03/18 13:13:02 by sancuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,26 +68,22 @@ size_t	word_len(const char *s, char del)
 size_t	arena_split(t_arena *dest, const char *src, char del)
 {
 	size_t	offset;
-	size_t	tmp_offset;
 	size_t	i;
 	size_t	word_cnt;
 	size_t	len;
 
 	word_cnt = count_words(src, del);
-	offset = arena_alloc(dest, (word_cnt + 1) * sizeof(size_t));
-	tmp_offset = offset;
+	offset = arena_alloc(dest, (word_cnt + 1) * sizeof(char *));
 	i = -1;
 	while (++i < word_cnt)
 	{
 		while (*src == del)
 			src++;
 		len = word_len(src, del);
-		dest->buf[tmp_offset]
-			= arena_strlcpy(dest, src, (len + 1) * sizeof(char));
+		((size_t *)(dest->buf + offset))[i] = arena_strlcpy(dest, src, (len + 1) * sizeof(char));
 		src += len + 1;
-		tmp_offset += sizeof(size_t);
 	}
-	dest->buf[tmp_offset] = arena_memset(dest, 0, sizeof(NULL)); // NULL ptr as terminator
+	arena_memset(dest, 0, sizeof(NULL)); // NULL ptr as terminator
 	return (offset);
 }
 
